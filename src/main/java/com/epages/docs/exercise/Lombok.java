837c2fe3687f96295690b29052d61d380057612f
Ulf Sauer <usauer@epages.com>
2016-06-23 14:09:09 +0200

    Add missing lombok task description

--- src/main/java/com/epages/docs/exercise/Lombok.java
+++ src/main/java/com/epages/docs/exercise/Lombok.java
@@ -14,6 +14,7 @@ import java.util.List;
  * - LineItem
  *   - is immutable
  *   - fields cannot be null
+ *   - two LineItems are equal when the contained Products are equal
  * - Order
  *   - is immutable
  *   - can only be created using a builder
